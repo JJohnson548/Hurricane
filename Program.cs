0d722caf2c501fe99327f66d5e50e1735fbec10b
johns <johns@JONSCOMPUTER>
2024-09-22 16:03:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+
+namespace Hurricane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter wind speed of storm in MPH");
+        
+            int windSpeed = int.Parse(Console.ReadLine());
+
+            if (windSpeed >= 157)
+            {
+
+                Console.WriteLine("Category Five Hurricane");
+            }
+
+            else if (windSpeed >= 130)
+            {
+                Console.WriteLine("Category Four Hurricane");
+            }
+            else if (windSpeed >= 111)
+            {
+                Console.WriteLine("Category Three Hurricane");
+            }
+
+            else if (windSpeed >= 96)
+            {
+                Console.WriteLine("Category Two Hurricane");
+            }
+
+            else if (windSpeed >= 74)
+            {
+                Console.WriteLine("Category One Hurricane");
+            }
+
+            else 
+            {
+
+                Console.WriteLine("Not a Hurricane");
+            
+            }
+
+
+        }
+    }
+}
